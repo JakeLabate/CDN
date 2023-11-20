251fdb4a8a0c92ff90427b9f5d0abbeb74824aba
Jake Labate <brandwield@gmail.com>
2023-11-20 11:38:23 -0500

    18 and 22 folders / script adjustment

--- custom-link-cursors/script.js
+++ custom-link-cursors/script.js
@@ -6,37 +6,30 @@ document.addEventListener("DOMContentLoaded", function () {
 
 	const links = document.querySelectorAll('a');
 	links.forEach(linkElement => {
+
+		// determine background color of link
 		const linkBackgroundColor = window.getComputedStyle(linkElement).backgroundColor;
 		const rgb = linkBackgroundColor.match(/\d+/g) || [255, 255, 255]; // Fallback to white if format is unexpected
 		const brightness = Math.round(((parseInt(rgb[0]) * 299) + (parseInt(rgb[1]) * 587) + (parseInt(rgb[2]) * 114)) / 1000);
 		const bgCategory = brightness < 125 ? 'light' : 'dark';
 
-		const linkType = determineLinkType(linkElement.href, currentDomain);
-		linkElement.style.cursor = `url('https://cdn.jakelabate.com/custom-link-cursors/cursors/${linkType}-${bgCategory}.svg'), auto`;
-		
+		// set link color
+		let linkColor;
+		switch(bgCategory) {
+			case 'dark': linkColor = 'light'; break;
+			case 'light': linkColor = 'dark'; break;
+		}
 
-		const scriptTag = document.getElementById('custom-link-cursors');
-		let cursorSize = scriptTag.getAttribute('data-cursor-size') || '22px';
+		// determine link type
+		const linkType = determineLinkType(linkElement.href, currentDomain);
+
+		// determine link size
+		const scriptTag = document.getElementById('custom-link-18px');
+		let cursorSize = scriptTag.getAttribute('data-cursor-size') || '22px'; // default to 22px
 		if (!cursorSize.endsWith('px')) cursorSize += 'px';
 
-		const svg = linkElement.style.cursor.split(',')[0].replace('url(', '');
-		
-		// get the html of the svg
-		fetch(svg).then(response => {
-			console.log(response);
-			return response.text();
-		}).then(svgHtml => {
-			// create a new div element
-			const div = document.createElement('div');
-			div.innerHTML = svgHtml.trim();
-			console.log(div);
-			// change the size of the svg
-			div.querySelector('svg').setAttribute('height', cursorSize);
-			div.querySelector('svg').setAttribute('width', cursorSize);
-			// replace the svg with the new svg
-			linkElement.style.cursor = `url('data:image/svg+xml;utf8,${div.innerHTML}'), auto`;
-			console.log(linkElement);
-		});
+		// set cursor
+		linkElement.style.cursor = `url('https://cdn.jakelabate.com/custom-link-cursors/cursors/${cursorSize}/${linkType}-${linkColor}.svg'), auto`;
 
 	});
 });
