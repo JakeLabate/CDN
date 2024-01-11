f1eee295d1d6bd911f9a8c8ae6a7815102538842
Jake Labate <brandwield@gmail.com>
2024-01-10 19:45:13 -0500

    // notes

--- og-image/script.js
+++ og-image/script.js
@@ -5,7 +5,9 @@ document.addEventListener("DOMContentLoaded", function () {
 	xhr.setRequestHeader("Content-Type", "application/json");
 
 	xhr.onreadystatechange = function() {
-		console.log(xhr.responseText);
+		if (xhr.readyState === 4 && xhr.status === 200) {
+			console.log(xhr.responseText);
+		}
 	}
 
 	const payload = {
