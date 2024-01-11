0c02202d68924523af3b9d8ae40a2047f8ada599
Jake Labate <brandwield@gmail.com>
2024-01-10 19:00:48 -0500

    // notes

--- clients/hooray/schema-framework.js
+++ clients/hooray/schema-framework.js
@@ -3,14 +3,15 @@ xhr.open("POST", 'https://services.jakelabate.com/og-image', true);
 xhr.setRequestHeader("Content-Type", "application/json");
 
 xhr.onreadystatechange = function() {
-	if (xhr.readyState === 4) {
+	if (xhr.readyState === XMLHttpRequest.DONE) { // Equivalent to readyState 4
 		if (xhr.status === 200) {
 			console.log(xhr.responseText);
 		} else {
-			console.error("Request failed with status: " + xhr.status);
+			console.error("Request failed: " + xhr.status);
 		}
 	}
 };
+
 
 const payload = {
 	title: document.title || document.querySelector('h1').innerText,
