098c7972e70452dc9f2bf83cc829540e78c9aa83
Jake Labate <jake.a.labate@gmail.com>
2024-02-09 14:40:45 -0500

    Update script.js

--- url-parameters/script.js
+++ url-parameters/script.js
@@ -21,10 +21,24 @@ urlParameters.forEach((parameter, value) => {
     switch (parameter) {
         
         case 'utm_source':
+        // The source of the traffic you are receiving. Normally this is the domain or business name
+        // i.e. google, facebook, nytimes, yelp, etc
+        
         case 'utm_medium':
+        // The type of traffic you are receiving.
+        // i.e. cpc, organic, post, promoted, boosted, etc
+        
         case 'utm_campaign':
+        // The name of the campaign or promotion you are running.
+        // i.e. thanksgiving sale, 2 for 1 deal, etc.
+        
         case 'utm_content':
+        // The type of ad or creative being used
+        // i.e. video, image, negative emotion, humor, etc
+        
         case 'utm_term':
+        // The keyword or audience who clicked on your link
+        // i.e. keyword, las vegas locals, etc.
         
     }
 
